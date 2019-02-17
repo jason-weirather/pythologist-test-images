7904825dc53fa28832de66d10e795b0a12fe65ea
Jason Weirather <jason-weirather@users.noreply.github.com>
2019-02-17 01:16:09 -0500

    add a small example

--- pythologisttestimages/__init__.py
+++ pythologisttestimages/__init__.py
@@ -1,6 +1,7 @@
 import os
 import pythologisttestimages
 from tempfile import TemporaryDirectory
+from pythologist.formats.inform.sets import CellProjectInForm
 class TestImages(object):
     def __init__(self):
         path = os.path.dirname(pythologisttestimages.__file__)
@@ -11,8 +12,10 @@ class TestImages(object):
         dirs = []
         for file in os.listdir(self._datadir):
             if not os.path.isdir(os.path.join(self._datadir,file)): 
-                raise ValueError("must be directories in data "+str(file))
+                continue
             dirs.append(file)
         return dirs
     def get_path(self,dataset):
         return os.path.join(self._datadir,dataset,'Example')
+    def get_pythologist(self,dataset):
+        return os.path.join(self._datadir,dataset,'pythologist.h5')
