609dd18df7f884407f33d56875c7f19e30450105
Jason Weirather <jason-weirather@users.noreply.github.com>
2019-02-21 07:55:39 -0500

    simplify test images

--- pythologisttestimages/__init__.py
+++ pythologisttestimages/__init__.py
@@ -1,26 +1,21 @@
 import os
-from pythologistreader.formats.inform.custom import CellProjectInFormLineArea
-from pythologistreader.formats.inform.sets import CellProjectInForm
 class TestImages(object):
     def __init__(self):
         self.base = os.path.abspath(os.path.join(__file__,'../../data'))
         return
     @property
-    def paths(self):
+    def raw(self):
         return {
             'IrisSpatialFeatures':os.path.join(self.base,'IrisSpatialFeatures','Example'),
             'Small':os.path.join(self.base,'Small','Example'),
             'Tiny':os.path.join(self.base,'Tiny','Example'),
         }
-    def pythologist(self,key):
+    @property
+    def pythologist(self):
         d = {
             'IrisSpatialFeatures':
                     os.path.join(self.base,'IrisSpatialFeatures','pythologist-tumor-margin.h5'),
             'Small':os.path.join(self.base,'Small','pythologist.h5'),
             'Tiny':os.path.join(self.base,'Tiny','pythologist_tiny.h5')
         }
-        if key not in d: raise ValueError("Must pick a key in "+str([x for x in d.keys()])) 
-        if key == 'IrisSpatialFeatures':
-            return CellProjectInFormLineArea(d[key],mode='r')
-        else:
-            return CellProjectInForm(d[key],mode='r')
+        return d
